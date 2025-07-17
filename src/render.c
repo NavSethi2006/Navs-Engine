2f0c51c0d3e6de2fa2eb41e0d80d2a9b86330f27
NavSethi2006 <navinsethi2006@gmail.com>
2025-07-17 00:08:49 -0400

    writing render

--- src/render.c
+++ src/render.c
@@ -1,27 +1,37 @@
 #include "render.h"
 
 
-Texture load_texture(const char* file_path, int x, int y, int width, int height) {
+Texture load_texture(const char* file_path, int width, int height, Window* window) {
     Texture texture;
-    texture.Image = IMG_LoadTexture()
+
+    int channels;
+
+    unsigned char* img_data = stbi_load(file_path, width, height, channels, 4);
+
+    if(!img_data) {
+        SDL_Log("Failed to load image: %s", stbi_failure_reason());
+    }
+
+    SDL_Surface* surface = SDL_CreateSurfaceFrom(img_data, width, height, SDL_PIXELFORMAT_RGBA32, width * 4);
+
+    if(!surface) {
+        SDL_Log("Failed to create surface: %s", SDL_GetError());
+        stbi_image_free(img_data);
+    }
+
+    texture.Image = SDL_CreateTextureFromSurface(window->renderer, surface);
+    SDL_DestroySurface(surface);
+    stbi_image_free(img_data);  
+    
+    return texture;
+
 }
 
 
 void render_texture(Texture *texture, Window *window) {
-    if (!texture || !window || !texture->sdl_texture) {
-        printf("Invalid texture or window for rendering.\n");
-        return;
-    }
 
-    SDL_Rect dst_rect = {
-        .x = (int)texture->x,
-        .y = (int)texture->y,
-        .w = (int)texture->width,
-        .h = (int)texture->height
-    };
-
-    // Render the texture to the window's renderer
-    if (SDL_RenderCopy(window->renderer, texture->sdl_texture, NULL, &dst_rect) < 0) {
-        printf("Failed to render texture: %s\n", SDL_GetError());
-    }
+    SDL_RenderTexture(window->renderer, texture->Image, NULL, NULL);
+    
 }
+
+
--- src/render.h
+++ src/render.h
@@ -1,20 +1,20 @@
 #ifndef RENDER_H
 #define RENDER_H
+#define STB_IMAGE_IMPLEMENTATION
 
 #include <SDL3/SDL.h>
 #include "window.h"
+#include <stb_image.h>
 #include <stdio.h>
 
 typedef struct Texture {
     SDL_Texture* Image;  /**< The SDL texture for rendering */
     float x;
     float y;
-    float width;                 /**< Width of the texture */
-    float height;                /**< Height of the texture */
 
 } Texture;
 
-Texture load_texture(const char* file_path, int x, int y, int width, int height);
+Texture load_texture(const char* file_path, int width, int height, Window* window);
 void render_texture(Texture *texture, Window *window);
 
 #endif // RENDER_H
